34d1d87e378787a785b0e41f66faca1cf8da0db5
Luiz pogi <johnluizvbangsoy14@gmail.com>
2026-02-23 21:07:20 +0800

    create a service for doctors

--- src/main/java/com/example/HospitalManagementAPI/entity/doctors.java
+++ src/main/java/com/example/HospitalManagementAPI/entity/doctors.java
@@ -35,20 +35,48 @@ public class doctors {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName_doc() {
         return name_doc;
+    }
+
+    public void setName_doc(String name_doc) {
+        this.name_doc = name_doc;
     }
 
     public String getSpecialization() {
         return specialization;
     }
 
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
     public String getContact_number() {
         return contact_number;
     }
 
+    public void setContact_number(String contact_number) {
+        this.contact_number = contact_number;
+    }
+
     public LocalDate getSchedule() {
         return schedule;
+    }
+
+    public void setSchedule(LocalDate schedule) {
+        this.schedule = schedule;
+    }
+
+    public List<appointments> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(List<appointments> appointments) {
+        this.appointments = appointments;
     }
 
 }
--- src/main/java/com/example/HospitalManagementAPI/service/doctorsService.java
+++ src/main/java/com/example/HospitalManagementAPI/service/doctorsService.java
@@ -1,4 +1,92 @@
 package com.example.HospitalManagementAPI.service;
 
+import com.example.HospitalManagementAPI.dto.doctors.doctorsRequest;
+import com.example.HospitalManagementAPI.dto.doctors.doctorsResponse;
+import com.example.HospitalManagementAPI.dto.patient.patientResponse;
+import com.example.HospitalManagementAPI.entity.doctors;
+import com.example.HospitalManagementAPI.repository.doctorsRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class doctorsService {
+
+    private final doctorsRepository repository;
+
+    public doctorsService(doctorsRepository repository) {
+        this.repository = repository;
+    }
+
+    public doctorsResponse createDoctor(doctorsRequest request) {
+        doctors entity = new doctors();
+        entity.setContact_number(request.getContact_number());
+        entity.setName_doc(request.getName_doc());
+        entity.setSchedule(request.getSchedule());
+        entity.setSpecialization(request.getSpecialization());
+
+        doctors doctorSaved = repository.save(entity);
+
+        doctorsResponse response = new doctorsResponse();
+        response.setId(doctorSaved.getId());
+        response.setContact_number(doctorSaved.getContact_number());
+        response.setName_doc(doctorSaved.getName_doc());
+        response.setSchedule(doctorSaved.getSchedule());
+        response.setSpecialization(doctorSaved.getSpecialization());
+
+        return response;
+    }
+
+    public List<doctorsResponse> viewAllDoctors() {
+        List<doctors> doctorsList = repository.findAll();
+
+        return doctorsList.stream().map(doctors -> {
+            doctorsResponse response = new doctorsResponse();
+            response.setId(doctors.getId());
+            response.setContact_number(doctors.getContact_number());
+            response.setName_doc(doctors.getName_doc());
+            response.setSchedule(doctors.getSchedule());
+            response.setSpecialization(doctors.getSpecialization());
+            return response;
+        }).toList();
+    }
+
+
+    public doctorsResponse updateDoctor(Long id, doctorsRequest request) {
+        doctors existingDoctors = repository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Doctors Not Found By ID" + id));
+
+        if(request.getName_doc() != null) {
+            existingDoctors.setName_doc(request.getName_doc());
+        }
+        if(request.getContact_number() != null) {
+            existingDoctors.setContact_number(request.getContact_number());
+        }
+        if(request.getSchedule() != null) {
+            existingDoctors.setSchedule(request.getSchedule());
+        }
+        if(request.getSpecialization() != null) {
+            existingDoctors.setSpecialization(request.getSpecialization());
+        }
+
+        doctors saved = repository.save(existingDoctors);
+
+        doctorsResponse response = new doctorsResponse();
+        response.setId(saved.getId());
+        response.setContact_number(saved.getContact_number());
+        response.setName_doc(saved.getName_doc());
+        response.setSchedule(saved.getSchedule());
+        response.setSpecialization(saved.getSpecialization());
+        return response;
+    }
+
+    public void deleteDoctorsById(Long id) {
+        if(!repository.existsById(id)) {
+            throw new RuntimeException("Doctors Not Found" + id);
+        }
+
+        repository.deleteById(id);
+    }
+
 }
